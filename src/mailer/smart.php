7034159469bd4d497e8ee62ed84265dcc62b0a98
Qumansa <qumansa@yandex.ru>
2020-07-13 18:31:54 +0400

    first commit

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'qumansa1@gmail.com';                 // Наш логин
+$mail->Username = '';                 // Наш логин
 $mail->Password = 'Onotole95!';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('qumansa1@gmail.com', 'Pulse');   // От кого письмо 
-$mail->addAddress('qumansa@yandex.ru'); 
+$mail->setFrom('');   // От кого письмо 
+$mail->addAddress(''); 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
